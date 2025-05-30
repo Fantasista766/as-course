1f06aa2fc93e6aed7a734abb8f3c01b70bfc44b1
fantasista766 <proxxx766@gmail.com>
2025-05-30 12:01:30 +0300

    create database.py

--- src/main.py
+++ src/main.py
@@ -11,9 +11,6 @@ import uvicorn
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
-from src.config import settings
-
-print(settings.DB_NAME)  # Example usage of settings
 
 app = FastAPI(docs_url=None, redoc_url=None)
 app.include_router(router_hotels)
