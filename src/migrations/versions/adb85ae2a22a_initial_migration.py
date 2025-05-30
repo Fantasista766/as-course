5e8bf37c5ce92f5a0548adf14affb7d50a106d4e
fantasista766 <proxxx766@gmail.com>
2025-05-30 12:40:53 +0300

    made first migration

---
+++ src/migrations/versions/adb85ae2a22a_initial_migration.py
@@ -0,0 +1,35 @@
+"""initial migration
+
+Revision ID: adb85ae2a22a
+Revises:
+Create Date: 2025-05-30 12:34:46.034434
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "adb85ae2a22a"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "hotels",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.Column("location", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("hotels")
