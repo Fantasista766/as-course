f0349dc26d099cf704ccda0f7df642c995aded17
fantasista766 <proxxx766@gmail.com>
2025-06-07 12:55:40 +0300

    add logout

--- src/api/auth.py
+++ src/api/auth.py
@@ -56,6 +56,12 @@ async def login_user(
     return {"status": "OK"}
 
 
+@router.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie("access_token")
+    return {"status": "OK"}
+
+
 @router.get("/me")
 async def get_me(user_id: UserIdDep):
     async with async_session_maker() as session:
