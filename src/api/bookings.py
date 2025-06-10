20d9c2c8683617980ca24330529129b2d5076249
Fantasista766 <proxxx766@gmail.com>
2025-06-10 17:06:37 +0300

    add get bookings

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -6,6 +6,19 @@ from src.schemas.bookings import Booking, BookingAdd, BookingAddRequest
 router = APIRouter(prefix="/bookings", tags=["Брони"])
 
 
+@router.get(
+    "/",
+    summary="Получить список бронирований",
+)
+async def get_bookings(db: DBDep) -> list[Booking]:
+    return await db.bookings.get_filtered()
+
+
+@router.get("/me", summary="Получить список бронирований пользователя")
+async def get_user_bookings(db: DBDep, user_id: UserIdDep) -> list[Booking]:
+    return await db.bookings.get_filtered(user_id=user_id)
+
+
 @router.post(
     "/",
     summary="Создать новую бронь",
@@ -20,7 +33,7 @@ async def create_booking(
                 "value": {
                     "room_id": 1,
                     "date_from": "2025-06-10",
-                    "date_to": "2025-06-10",
+                    "date_to": "2025-06-20",
                 },
             },
         }
