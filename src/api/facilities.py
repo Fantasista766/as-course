e17f95a6eaf32e01b937beaf536976be1e174c98
fantasista766 <proxxx766@gmail.com>
2025-06-18 12:02:34 +0300

    add redis for facilities

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -1,6 +1,9 @@
+import json
+
 from fastapi import APIRouter, Body
 
 from src.api.dependencies import DBDep
+from src.init import redis_manager
 from src.schemas.facilities import Facility, FacilityAdd
 
 router = APIRouter(prefix="/facilities", tags=["Удобства"])
@@ -8,7 +11,17 @@ router = APIRouter(prefix="/facilities", tags=["Удобства"])
 
 @router.get("/", summary="Получить список удобств")
 async def get_facilities(db: DBDep) -> list[Facility]:
-    return await db.facilities.get_all()
+    facilities_from_cache = await redis_manager.get("facilities")
+    if not facilities_from_cache:
+        print("BASE")
+        facilities = await db.facilities.get_all()
+        facilities_schemas = [f.model_dump() for f in facilities]
+        facilities_json = json.dumps(facilities_schemas)
+        await redis_manager.set("facilities", facilities_json, 10)
+        return facilities
+
+    facilites_dicts = json.loads(facilities_from_cache)
+    return facilites_dicts
 
 
 @router.post("/", summary="Создать новое удобство")
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from pathlib import Path
 import sys
 
@@ -11,8 +12,17 @@ from src.api.bookings import router as router_bookings
 from src.api.facilities import router as router_facilities
 from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
+from src.init import redis_manager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await redis_manager.connect()
+    yield
+    await redis_manager.close()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router_auth)
 app.include_router(router_bookings)
 app.include_router(router_facilities)
