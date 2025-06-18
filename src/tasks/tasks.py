26072cd746302b215cb3faba3a0bcc9e37ef0057
Fantasista766 <proxxx766@gmail.com>
2025-06-18 22:27:51 +0300

    small adjustments for resize image

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -14,7 +14,7 @@ def test_task() -> None:
 
 @celery_instance.task  # type: ignore
 def resize_image(image_path: str):
-    sizes = [200, 500, 1000]
+    widths = [100, 500, 720, 1920, 3840]
     output_dir = "src/static/images"
 
     img = Image.open(image_path)
@@ -22,15 +22,15 @@ def resize_image(image_path: str):
     base_name = os.path.basename(image_path)
     name, ext = os.path.splitext(base_name)
 
-    for size in sizes:
+    for width in widths:
         img_resized = img.resize(  # type: ignore
-            (size, int(img.height * (size / img.width))), Image.Resampling.LANCZOS
+            (width, int(img.height * (width / img.width))), Image.Resampling.LANCZOS
         )
 
-        new_file_name = f"{name}_{size}px{ext}"
+        new_file_name = f"{name}_{width}px{ext}"
 
         output_path = os.path.join(output_dir, new_file_name)
 
         img_resized.save(output_path)
 
-    print(f"Изображние сохранено в размерах {sizes} в папке {output_dir}")
+    print(f"Изображние сохранено в размерах {widths} в папке {output_dir}")
