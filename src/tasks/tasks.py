028ba9348960da234c16f47bd46c1d1205aef0a0
fantasista766 <proxxx766@gmail.com>
2025-06-18 18:00:07 +0300

    celery image task

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,4 +1,7 @@
 from time import sleep
+import os
+
+from PIL import Image
 
 from src.tasks.celery_app import celery_instance
 
@@ -7,3 +10,27 @@ from src.tasks.celery_app import celery_instance
 def test_task() -> None:
     sleep(5)
     print("GOOD JOB")
+
+
+@celery_instance.task  # type: ignore
+def resize_image(image_path: str):
+    sizes = [200, 500, 1000]
+    output_dir = "src/static/images"
+
+    img = Image.open(image_path)
+
+    base_name = os.path.basename(image_path)
+    name, ext = os.path.splitext(base_name)
+
+    for size in sizes:
+        img_resized = img.resize(  # type: ignore
+            (size, int(img.height * (size / img.width))), Image.Resampling.LANCZOS
+        )
+
+        new_file_name = f"{name}_{size}px{ext}"
+
+        output_path = os.path.join(output_dir, new_file_name)
+
+        img_resized.save(output_path)
+
+    print(f"Изображние сохранено в размерах {sizes} в папке {output_dir}")
