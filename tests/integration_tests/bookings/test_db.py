05652fc5f4fc0f43a6c12d0989e5f41ec6ea8e45
fantasista766 <proxxx766@gmail.com>
2025-06-20 16:42:52 +0300

    small adjustments for bookings crud

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -9,23 +9,26 @@ async def test_booking_crud(db: DBManager):
     room_id = (await db.rooms.get_all())[0].id
     create_at = datetime.now()
 
+    create_date_from = date(year=2025, month=12, day=12)
+    create_date_to = date(year=2025, month=12, day=22)
+
     # create data
     booking_data = BookingAddRequest(
         user_id=user_id,  # type: ignore
         room_id=room_id,
-        date_from=date(year=2025, month=12, day=12),
-        date_to=date(year=2025, month=12, day=22),
+        date_from=create_date_from,
+        date_to=create_date_to,
         price=100,  # type: ignore
         create_at=create_at,  # type: ignore
     )
-    await db.bookings.add(booking_data)
+    new_booking = await db.bookings.add(booking_data)
 
     # read data
-    read_booking_data = (await db.bookings.get_all())[-1]
+    read_booking_data = await db.bookings.get_one_or_none(id=new_booking.id)
     assert read_booking_data.user_id == user_id
     assert read_booking_data.room_id == room_id
-    assert read_booking_data.date_from == date(year=2025, month=12, day=12)
-    assert read_booking_data.date_to == date(year=2025, month=12, day=22)
+    assert read_booking_data.date_from == create_date_from
+    assert read_booking_data.date_to == create_date_to
     assert read_booking_data.price == 100
     assert read_booking_data.create_at == create_at
 
