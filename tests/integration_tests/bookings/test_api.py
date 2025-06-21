dff635affeb0b743e0d2d45787c9cb9dc590749e
fantasista766 <proxxx766@gmail.com>
2025-06-21 16:22:50 +0300

    parametrized test add and get bookings

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,3 +1,5 @@
+from typing import Any, Callable
+
 from httpx import AsyncClient
 import pytest
 
@@ -21,10 +23,8 @@ async def test_add_booking(
     date_from: str,
     date_to: str,
     status_code: int,
-    db: DBManager,
     authenticated_ac: AsyncClient,
 ):
-    # room_id = (await db.rooms.get_all())[0].id
     response = await authenticated_ac.post(
         "/bookings",
         json={"room_id": room_id, "date_from": date_from, "date_to": date_to},
@@ -35,3 +35,44 @@ async def test_add_booking(
         assert isinstance(res, dict)
         assert res["status"] == "OK"
         assert res["data"]["room_id"] == room_id
+
+
+@pytest.fixture(scope="function")
+async def delete_all_bookings(db: DBManager, authenticated_ac: AsyncClient):
+    response = await authenticated_ac.get("/auth/me")
+    user_id = response.json()["id"]
+    response = await db.bookings.get_filtered(user_id=user_id)
+    ids_to_delete = [booking.id for booking in response]
+    await db.bookings.delete_batch_by_ids(ids_to_delete=ids_to_delete)
+    await db.commit()
+
+
+@pytest.mark.parametrize(
+    "room_id, date_from, date_to, status_code, bookings_amount",
+    [
+        (1, "2025-06-21", "2025-06-28", 200, 1),
+        (1, "2025-06-22", "2025-06-29", 200, 1),
+        (1, "2025-06-23", "2025-06-30", 200, 1),
+    ],
+)
+async def test_add_and_get_my_bookings(
+    room_id: int,
+    date_from: str,
+    date_to: str,
+    status_code: int,
+    bookings_amount: int,
+    authenticated_ac: AsyncClient,
+    delete_all_bookings: Callable[..., Any],
+):
+    # добавили бронь
+    response = await authenticated_ac.post(
+        "/bookings",
+        json={"room_id": room_id, "date_from": date_from, "date_to": date_to},
+    )
+    assert response.status_code == status_code
+
+    response = await authenticated_ac.get(
+        "/bookings/me",
+    )
+    assert response.status_code == status_code
+    assert len(response.json()) == bookings_amount
