01b320c3ba111cf3456071785d61a658ac033a0e
fantasista766 <proxxx766@gmail.com>
2025-06-21 11:50:04 +0300

    beautify tests

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -2,12 +2,16 @@ from httpx import AsyncClient
 
 
 async def test_add_facility(ac: AsyncClient):
-    response = await ac.post("/facilities", json={"title": "Интернет"})
-    print(f"{response.json()=}")
+    facility_title = "Интернет"
+    response = await ac.post("/facilities", json={"title": facility_title})
     assert response.status_code == 200
+    res = response.json()
+    assert isinstance(res, dict)
+    assert "data" in res
+    assert res["data"]["title"] == facility_title
 
 
 async def test_get_facilities(ac: AsyncClient):
     response = await ac.get("/facilities")
-    print(f"{response.json()=}")
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
--- tests/integration_tests/hotels/test_api.py
+++ tests/integration_tests/hotels/test_api.py
@@ -5,5 +5,5 @@ async def test_get_hotels(ac: AsyncClient):
     response = await ac.get(
         "/hotels", params={"date_from": "2025-06-21", "date_to": "2025-06-28"}
     )
-    print(f"{response.json()=}")
     assert response.status_code == 200
+    assert isinstance(response.json(), list)
