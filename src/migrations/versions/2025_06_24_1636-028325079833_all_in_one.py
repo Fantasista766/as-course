0ee3a381c650b3a996b649ca068209705c34d0ca
fantasista766 <proxxx766@gmail.com>
2025-06-26 12:44:00 +0300

    return migraions

---
+++ src/migrations/versions/2025_06_24_1636-028325079833_all_in_one.py
@@ -0,0 +1,103 @@
+"""all_in_one
+
+Revision ID: 028325079833
+Revises:
+Create Date: 2025-06-24 16:36:34.329461
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "028325079833"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "hotels",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.Column("location", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("first_name", sa.String(length=50), nullable=False),
+        sa.Column("last_name", sa.String(length=50), nullable=False),
+        sa.Column("email", sa.String(length=200), nullable=False),
+        sa.Column("hashed_password", sa.String(length=200), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("email"),
+    )
+    op.create_table(
+        "rooms",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("hotel_id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(), nullable=False),
+        sa.Column("description", sa.String(), nullable=True),
+        sa.Column("price", sa.Integer(), nullable=False),
+        sa.Column("quantity", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["hotel_id"],
+            ["hotels.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "bookings",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("date_from", sa.Date(), nullable=False),
+        sa.Column("date_to", sa.Date(), nullable=False),
+        sa.Column("price", sa.Integer(), nullable=False),
+        sa.Column("create_at", sa.DateTime(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "rooms_facilities",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("room_id", sa.Integer(), nullable=False),
+        sa.Column("facility_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["facility_id"],
+            ["facilities.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["room_id"],
+            ["rooms.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("rooms_facilities")
+    op.drop_table("bookings")
+    op.drop_table("rooms")
+    op.drop_table("users")
+    op.drop_table("hotels")
+    op.drop_table("facilities")
